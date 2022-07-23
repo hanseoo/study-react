93acda302eeb80a098e784794d29fce8592d440e
hanseoo <trullo494@gmail.com>
2022-07-23 15:42:28 +0900

    ref를 사용해야 하는 상황 - DOM을 직접적으로 건드려야 할 때 이다.   - state만으로 해결할 수 없는 상황     - 특정 input에 포커스 주기     - 스크롤 박스 조작     - canvas 요소에 그림 그리기

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import EventPractice from './EventPractice';
+import ValidationSample from "./ValidationSample";
 
 const App = () => {
-  return <EventPractice />;
+  return <ValidationSample />;
 };
 
 export default App;
